7b31b0f49499e47db9c2c9cfdd8da26383f51dfa
kartikpavan <kartik.varanasi@yahoo.com>
2022-08-11 23:37:54 +0530

    sd

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { firebaseApp } from "../utils/firebase.config";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-
 import { BsGoogle } from "react-icons/bs";
 
 const Login = () => {
